5a6e9f2aaf4c04461c26204feea920eb978a4743
github username <hhl262@nyu.edu>
2015-12-12 17:28:02 -0500

    can find closet name

--- app/models/closet.js
+++ app/models/closet.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  username: DS.attr('string'),
-  email: DS.attr('string'),
+  name: DS.attr('string'),
   // closets: DS.hasMany('closet', { async: true }),
 });
