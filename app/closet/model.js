1f1da08db2287d4ea46d5a7109db3b24e4b62977
HideyukiGojima <hgojima@gmail.com>
2015-12-12 20:33:38 -0500

    items basic setup

--- app/closet/model.js
+++ app/closet/model.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   boardId: DS.attr('number'),
   userId: DS.attr('number'),
-  user: DS.belongsTo('user')
+  user: DS.belongsTo('user'),
+  // items: DS.hasMany('item', { async: true })
 });
