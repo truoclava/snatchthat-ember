9eb46fdba654dd4c36a9924d2ab45d6d4eeeb0b7
angamaiton <ofurmadur@icloud.com>
2015-12-13 20:35:43 -0500

    dashboard not working, but routes enabled

--- app/router.js
+++ app/router.js
@@ -8,7 +8,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('index');
   this.route('login');
-  this.route('dashboard', {path: '/'});
+  this.route('dashboard', {path: '/'}); //authenticated root
+  this.route('signup'); //unauthenticated root
   this.resource('users', function() {
     this.route('user', { path: '/:user_id'}, function() {
       this.resource('closets', function() {
